ebac644b7147433d43b4f6e6cb25c63699d0bfda
윤석 이 <nuclearbomb280@gmail.com>
2025-05-08 15:43:58 +0900

    feature : added KeyCodeData Scriptable Object

---
+++ Project/RelicsOfTheRuins/Assets/Scripts/ScriptableObjects/KeyCodeData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RelicsOfTheRuins.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "KeyCodeData", menuName = "ScriptableObjects/KeyCodeData", order = 1)]
+    public class KeyCodeData : ScriptableObject
+    {
+        //나중에 이런 형태로 키 더 추가하세요
+        public KeyCode test1;
+    }
+}
