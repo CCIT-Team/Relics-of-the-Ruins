40346af8dcdfc4626e9ad1a1d9920cbfed7bfe5a
윤석 이 <nuclearbomb280@gmail.com>
2025-05-09 17:58:35 +0900

    feature : added ExplorerDataHub and PlayerScreenCanvasPrefabs

--- Project/RelicsOfTheRuins/Assets/Scripts/ScriptableObjects/KeyCodeData.cs
+++ Project/RelicsOfTheRuins/Assets/Scripts/ScriptableObjects/KeyCodeData.cs
@@ -8,7 +8,6 @@ namespace RelicsOfTheRuins.ScriptableObjects
     [CreateAssetMenu(fileName = "KeyCodeData", menuName = "ScriptableObjects/KeyCodeData", order = 1)]
     public class KeyCodeData : ScriptableObject
     {
-        //나중에 이런 형태로 키 더 추가하세요
-        public KeyCode test1;
+        
     }
 }
